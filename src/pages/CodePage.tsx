443d0baaa3fa10c959f89ccf2b57dbae77e08307
Waled Khatiz <waledkhatiz@Waleds-MacBook-Air.local>
2024-03-10 16:27:11 +1100

    code page placeholder inserted

--- src/pages/CodePage.tsx
+++ src/pages/CodePage.tsx
@@ -1,7 +1,8 @@
 function CodePage() {
   return (
-    <div className="flex-1 bg-cyan-500">
-      <h1>Code Page Is Selected</h1>
+    <div className="flex flex-1 flex-col items-center justify-center overflow-y-scroll p-7 md:overflow-y-auto lg:p-14 xl:p-24 xl:px-56">
+      <img className="mb-3 h-[100px] w-[100px]" src="/construction.png"></img>
+      <h1 className="text-2xl font-bold text-sky-900">Under Construction</h1>
     </div>
   );
 }
